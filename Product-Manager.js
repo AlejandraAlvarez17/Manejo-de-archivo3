d6d3df1f4c0c93281caeee4c0d517d39d9f28eb5
AlejandraAlvarez17 <116087716+AlejandraAlvarez17@users.noreply.github.com>
2024-02-14 00:57:37 +0100

    Add files via upload

--- Product-Manager.js
+++ Product-Manager.js
@@ -1,10 +1,17 @@
 const fs = require("fs").promises;
 
-const  productManager= new ProductManager();
-productManager.addProduct("Impresora Hp","Impresora multifuncion 2700",3800,"impresora.jpg","HP2700",2000);
-productManager.addProduct("Auriculares gammer","Gammer360 grados",360,"Auriculares.jpg","Sirie780",100);
-productManager.addProduct("Teclado Gammer","Teclado Ultra",500,"Teclado.jpg","Tec548",10)
-  
+//const  productManager= new ProductManager();
+//productManager.addProduct("Impresora Hp","Impresora multifuncion 2700",3800,"impresora.jpg","HP2700",2000);
+//productManager.addProduct("Auriculares gammer","Gammer360 grados",360,"Auriculares.jpg","Sirie780",100);
+//productManager.addProduct("Teclado Gammer","Teclado Ultra",500,"Teclado.jpg","Tec548",10)
+
+  class ProductManager{
+    constructor (){
+        this.path = "./product.json" ;
+
+    }
+  }
+
  crearProducts = async(product) =>{
     try{
         const productsNew = await this.readProducts();
@@ -22,7 +29,7 @@ const archivoProduct = "./archivoProduct.json";
 const guardarArchivos = async() => {
     try{
 
-    await fs.promises.writeFile(archivoProducto,json.stringify(productManager, null, 2));
+    await fs.promises.writeFile(archivoProduct,json.stringify(productManager, null, 2));
  } catch(error){
    console.log("Error al guardar",error);
  }
